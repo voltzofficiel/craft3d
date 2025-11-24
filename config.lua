d92873d5548e57ecd7aa945bfe1f612dd2747906
voltzofficiel <teamvoltzofficiel@gmail.com>
2025-11-24 08:53:46 +0100

    Add files via upload

--- config.lua
+++ config.lua
@@ -5,39 +5,20 @@ Config.Locale = 'fr'
 Config.Tables = {
     {
         model = `prop_tool_bench02_ld`,
-        coords = vector4(-518.18, -171.05, 37.65, 28.0),
+        coords =  vector4(4442.35, -4467.08, 4.33, 195.86),
         radius = 2.0
     }
 }
 
 Config.Recipes = {
     {
-        label = 'Kit de réparation',
-        result = { item = 'fixkit', count = 1 },
+        label = 'Kit de sultan',
+        result = { item = 'sultan', count = 1 },
         requirements = {
-            { item = 'metal', count = 5 },
-            { item = 'plastic', count = 2 }
+            { item = 'wood', count = 5 },
         },
-        time = 5000
+        time = 1000
     },
-    {
-        label = 'Chargeur bricolé',
-        result = { item = 'clip', count = 1 },
-        requirements = {
-            { item = 'scrapmetal', count = 8 },
-            { item = 'spring', count = 2 }
-        },
-        time = 7500
-    },
-    {
-        label = 'Trousse médicale',
-        result = { item = 'medikit', count = 1 },
-        requirements = {
-            { item = 'bandage', count = 2 },
-            { item = 'alcohol', count = 1 }
-        },
-        time = 6000
-    }
 }
 
 Config.UseProgressBar = true -- si vous avez un export de barre de progression type ox_lib / rprogress
